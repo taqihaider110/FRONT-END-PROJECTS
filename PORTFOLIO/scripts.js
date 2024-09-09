5089702af748c4987d6a4edd92a541bbba4cd017
taqihaider591 <taqihaider591@gmail.com>
2024-09-10 01:02:58 +0500

    New Project

--- PORTFOLIO/scripts.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.getElementById('menu-toggle');
-    const navbar = document.getElementById('navbar');
-
-    menuToggle.addEventListener('click', function () {
-        navbar.classList.toggle('active');
-    });
-});
